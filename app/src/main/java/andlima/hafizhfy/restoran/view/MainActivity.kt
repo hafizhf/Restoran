aaabfce6b744134497387b7693a8cf172cf623b8
hafizhf <hafizhfy@upi.edu>
2022-05-19 14:07:06 +0700

    New seller and user variants

--- app/src/main/java/andlima/hafizhfy/restoran/view/MainActivity.kt
+++ app/src/main/java/andlima/hafizhfy/restoran/view/MainActivity.kt
@@ -3,10 +3,16 @@ package andlima.hafizhfy.restoran.view
 import andlima.hafizhfy.restoran.R
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
     }
 }
